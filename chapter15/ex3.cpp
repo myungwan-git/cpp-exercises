29fda7c5754ef4ab5d475b94b8e0d2666b21c43c
myungwan-git <nsubway1070635@gmail.com>
2022-04-15 00:06:44 +0900

    template ex3 modify

--- chapter15/ex3.cpp
+++ chapter15/ex3.cpp
@@ -7,17 +7,33 @@ class Oper {
         long int y;
         double z;
     public:
-        template<typename T> T res(T x);
+        template <typename T, std::size_t N> void res(T (&x)[N]);
 };
 
-template<typename T>
-T Oper::res(T x) {
+template <typename T, std::size_t N>
+void Oper::res(T (&x)[N]) {
+    static_assert(N>0, "배열의 크기는 0 이상이어야 합니다.");
+    T result;
+    for(auto && var : x) {
+        cout << var << " ";
+        result += var;
+    }
+    
+    cout << endl;
+
+    cout << sizeof(x) << endl;
+    cout << "result = " << result << endl;
     
 }
 
+
 int main() {
+    int arr[3] = {1,2,3};
+    Oper oper;
+    oper.res(arr);
 
-
+    double dArr[3] = {1.44,2.14, 3.11};
+    oper.res(dArr);
 
     return 0;
 }
